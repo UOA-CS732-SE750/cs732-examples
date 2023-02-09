cb7fd86eef40baca7eb564a59128a1ed76319f1a
Andrew Meads <andrew.meads@auckland.ac.nz>
2023-02-09 14:19:18 +1300

    Updated example 18 to latest Express and ES modules

--- example-18-routes-fetch-axios/server.js
+++ example-18-routes-fetch-axios/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import * as url from 'url';
 
 // Setup Express
 const app = express();
@@ -13,7 +14,7 @@ import routes from './routes';
 app.use('/', routes);
 
 // Make the "public" folder available statically
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(url.fileURLToPath(new URL('.', import.meta.url)), 'public')));
 
 // Start the server running. Once the server is running, the given function will be called, which will
 // log a simple message to the server console. Any console.log() statements in your node.js code
