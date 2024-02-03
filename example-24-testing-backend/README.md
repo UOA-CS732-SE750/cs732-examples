f8f69188d8e789471ee2c8b38772b81ddb938f33
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-02-03 13:57:11 +1300

    Updated example 24 to 2024 code style and package versions

--- example-24-testing-backend/README.md
+++ example-24-testing-backend/README.md
@@ -1,10 +1,11 @@
 # CS732 examples - Testing backend code (Express and mongoose)
+
 This project continues on from the previous example by showing how we can test our Express routes and our code which interacts with a MongoDB database.
 
-
 ## Testing a MongoDB schema
-[schema.test.js](./src/db/__tests__/schema.test.js) tests that the `Breakfast` schema defined in [schema.js](./src/db/schema.js) is correct. It does this by creating an running an in-memory MongoDB instance using the `mongodb-memory-server` package. We can see how this is created in the `beforeAll()` method on line 12, and shutdown in the `afterAll()` method on line 57. Additionally, we populate the in-memory database with data before each test is run (line 24), and clear the database after each test (line 50). Note that the code in these `beforeEach()` and `afterEach()` functions doesn't use the `Breakfast` schema under test - it uses the native MongoDB functions for adding / clearing data.
 
+[schema.test.js](./src/db/__tests__/schema.test.js) tests that the `Breakfast` schema defined in [schema.js](./src/db/schema.js) is correct. It does this by creating an running an in-memory MongoDB instance using the `mongodb-memory-server` package. We can see how this is created in the `beforeAll()` method on line 33, and shutdown in the `afterAll()` method on line 54. Additionally, clear the database and re-initialize it with dummy data before we run each test (line 43). Note that the code in `beforeEach()` doesn't use the `Breakfast` schema under test - it uses the native MongoDB functions for adding / clearing data.
 
 ## Testing an Express server
-[breakfast.test.js](./src/routes/__tests__/breakfast.test.js) tests the Express routes defined in [breakfast.js](./src/routes/breakfast.js). Like the `schema.test.js` tests above, we also create an in-memory MongoDB instance. This time, we also create an Express server, and configure it with the routes we're trying to test (line 9). We then use the [`supertest` package](https://www.npmjs.com/package/supertest) to make requests. Supertest will take care of starting and stopping the Express server as needed, and will manage ports, etc. for us. The tests themselves examine whether the Express routes correctly serve up the data contained within the database as expected.
+
+[breakfast.test.js](./src/routes/__tests__/breakfast.test.js) tests the Express routes defined in [breakfast.js](./src/routes/breakfast.js). Like the `schema.test.js` tests above, we also create an in-memory MongoDB instance. This time, we also create an Express server, and configure it with the routes we're trying to test (line 10). We then use the [`supertest` package](https://www.npmjs.com/package/supertest) to make requests. Supertest will take care of starting and stopping the Express server as needed, and will manage ports, etc. for us. The tests themselves examine whether the Express routes correctly serve up the data contained within the database as expected.
--- example-24-testing-backend/src/db/__tests__/schema.test.js
+++ example-24-testing-backend/src/db/__tests__/schema.test.js
@@ -1,27 +1,27 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import Breakfast from '../schema';
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import Breakfast from "../schema";
 
 let mongod;
 
 const breakfast1 = {
-    _id: new mongoose.Types.ObjectId('000000000000000000000001'),
-    eggs: 7,
-    bacon: 10,
-    drink: 'Coffee'
+  _id: new mongoose.Types.ObjectId("000000000000000000000001"),
+  eggs: 7,
+  bacon: 10,
+  drink: "Coffee"
 };
 
 const breakfast2 = {
-    _id: new mongoose.Types.ObjectId('000000000000000000000002'),
-    eggs: 12,
-    bacon: 2,
+  _id: new mongoose.Types.ObjectId("000000000000000000000002"),
+  eggs: 12,
+  bacon: 2
 };
 
 const breakfast3 = {
-    _id: new mongoose.Types.ObjectId('000000000000000000000003'),
-    eggs: 8,
-    bacon: 50,
-    drink: 'Tea'
+  _id: new mongoose.Types.ObjectId("000000000000000000000003"),
+  eggs: 8,
+  bacon: 50,
+  drink: "Tea"
 };
 
 const breakfasts = [breakfast1, breakfast2, breakfast3];
@@ -31,122 +31,120 @@ const breakfasts = [breakfast1, breakfast2, breakfast3];
  * then establish a mongoose connection to it.
  */
 beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
 
-    mongod = await MongoMemoryServer.create();
-
-    const connectionString = mongod.getUri();
-    await mongoose.connect(connectionString, { useNewUrlParser: true });
-
+  const connectionString = mongod.getUri();
+  await mongoose.connect(connectionString);
 });
 
 /**
  * Before each test, intialize the database with some data
  */
 beforeEach(async () => {
+  // Drop existing collections
+  await mongoose.connection.db.dropDatabase();
 
-    // Drop existing collections
-    await mongoose.connection.db.dropDatabase();
-
-    const coll = await mongoose.connection.db.createCollection('breakfasts');
-    await coll.insertMany(breakfasts);
+  const coll = await mongoose.connection.db.createCollection("breakfasts");
+  await coll.insertMany(breakfasts);
 });
 
 /**
  * After all tests, gracefully terminate the in-memory MongoDB instance and mongoose connection.
  */
 afterAll(async () => {
-    await mongoose.disconnect();
-    await mongod.stop();
+  await mongoose.disconnect();
+  await mongod.stop();
 });
 
-it('gets breakfasts', async () => {
+it("gets breakfasts", async () => {
+  const breakfastsFromDb = await Breakfast.find();
+  expect(breakfastsFromDb).toBeTruthy();
+  expect(breakfastsFromDb.length).toBe(3);
 
-    const breakfastsFromDb = await Breakfast.find();
-    expect(breakfastsFromDb).toBeTruthy();
-    expect(breakfastsFromDb.length).toBe(3);
+  expect(breakfastsFromDb[0].eggs).toBe(7);
+  expect(breakfastsFromDb[0].bacon).toBe(10);
+  expect(breakfastsFromDb[0].drink).toBe("Coffee");
 
-    expect(breakfastsFromDb[0].eggs).toBe(7);
-    expect(breakfastsFromDb[0].bacon).toBe(10);
-    expect(breakfastsFromDb[0].drink).toBe('Coffee');
+  expect(breakfastsFromDb[1].eggs).toBe(12);
+  expect(breakfastsFromDb[1].bacon).toBe(2);
+  expect(breakfastsFromDb[1].drink).toBeUndefined();
 
-    expect(breakfastsFromDb[1].eggs).toBe(12);
-    expect(breakfastsFromDb[1].bacon).toBe(2);
-    expect(breakfastsFromDb[1].drink).toBeUndefined();
-
-    expect(breakfastsFromDb[2].eggs).toBe(8);
-    expect(breakfastsFromDb[2].bacon).toBe(50);
-    expect(breakfastsFromDb[2].drink).toBe('Tea');
+  expect(breakfastsFromDb[2].eggs).toBe(8);
+  expect(breakfastsFromDb[2].bacon).toBe(50);
+  expect(breakfastsFromDb[2].drink).toBe("Tea");
 });
 
-it('gets a single breakfast', async () => {
-    const breakfast = await Breakfast.findById('000000000000000000000002');
-    expect(breakfast.eggs).toBe(12);
-    expect(breakfast.bacon).toBe(2);
-    expect(breakfast.drink).toBeUndefined();
+it("gets a single breakfast", async () => {
+  const breakfast = await Breakfast.findById("000000000000000000000002");
+  expect(breakfast.eggs).toBe(12);
+  expect(breakfast.bacon).toBe(2);
+  expect(breakfast.drink).toBeUndefined();
 });
 
-it('adds a breakfast without crashing', async () => {
-    const breakfast = new Breakfast({
-        eggs: 9,
-        bacon: 2,
-        drink: 'Tea'
-    });
+it("adds a breakfast without crashing", async () => {
+  const breakfast = new Breakfast({
+    eggs: 9,
+    bacon: 2,
+    drink: "Tea"
+  });
 
-    await breakfast.save();
+  await breakfast.save();
 
-    const fromDb = await mongoose.connection.db.collection('breakfasts').findOne({ _id: breakfast._id });
-    expect(fromDb).toBeTruthy();
-    expect(fromDb.eggs).toBe(9);
-    expect(fromDb.bacon).toBe(2);
-    expect(fromDb.drink).toBe('Tea');
+  const fromDb = await mongoose.connection.db
+    .collection("breakfasts")
+    .findOne({ _id: breakfast._id });
+  expect(fromDb).toBeTruthy();
+  expect(fromDb.eggs).toBe(9);
+  expect(fromDb.bacon).toBe(2);
+  expect(fromDb.drink).toBe("Tea");
 });
 
-it('fails when adding too few eggs', () => {
-    const breakfast = new Breakfast({
-        eggs: 5,
-        bacon: 2,
-        drink: 'Tea'
-    });
+it("fails when adding too few eggs", () => {
+  const breakfast = new Breakfast({
+    eggs: 5,
+    bacon: 2,
+    drink: "Tea"
+  });
 
-    return expect(breakfast.save()).rejects.toThrow();
+  return expect(breakfast.save()).rejects.toThrow();
 });
 
-it('fails when adding too many eggs', () => {
-    const breakfast = new Breakfast({
-        eggs: 13,
-        bacon: 2,
-        drink: 'Tea'
-    });
+it("fails when adding too many eggs", () => {
+  const breakfast = new Breakfast({
+    eggs: 13,
+    bacon: 2,
+    drink: "Tea"
+  });
 
-    return expect(breakfast.save()).rejects.toThrow();
+  return expect(breakfast.save()).rejects.toThrow();
 });
 
-it('fails when forgetting to add bacon', () => {
-    const breakfast = new Breakfast({
-        eggs: 7,
-        drink: 'Tea'
-    });
+it("fails when forgetting to add bacon", () => {
+  const breakfast = new Breakfast({
+    eggs: 7,
+    drink: "Tea"
+  });
 
-    return expect(breakfast.save()).rejects.toThrow();
+  return expect(breakfast.save()).rejects.toThrow();
 });
 
-it('fails when forgetting to add a drink with enough bacon', () => {
-    const breakfast = new Breakfast({
-        eggs: 7,
-        bacon: 4
-    });
+it("fails when forgetting to add a drink with enough bacon", () => {
+  const breakfast = new Breakfast({
+    eggs: 7,
+    bacon: 4
+  });
 
-    return expect(breakfast.save()).rejects.toThrow();
+  return expect(breakfast.save()).rejects.toThrow();
 });
 
-it('fails when adding an incorrect kind of drink', () => {
-    const breakfast = new Breakfast({
-        eggs: 7,
-        bacon: 1,
-        drink: 'Milkshake'
-    });
+it("fails when adding an incorrect kind of drink", () => {
+  const breakfast = new Breakfast({
+    eggs: 7,
+    bacon: 1,
+    drink: "Milkshake"
+  });
 
-    return expect(breakfast.save()).rejects.toThrow();
+  return expect(breakfast.save()).rejects.toThrow();
 });
 
 // etc...
